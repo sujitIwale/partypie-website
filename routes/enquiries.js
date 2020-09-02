23a106a2fc244b97dcc4bc6e8a5711f756220346
sujitIwale <56259235+sujitIwale@users.noreply.github.com>
2020-09-02 16:30:08 +0530

    update1

--- routes/enquiries.js
+++ routes/enquiries.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-// const { check, validationResult } = require("express-validator");
+const { check, validationResult } = require("express-validator");
 
 const Enquiry = require("../models/Enquiries");
 
@@ -10,12 +10,12 @@ const Enquiry = require("../models/Enquiries");
 
 router.post(
   "/enquiries",
-  // [check("name", "Please Provide Your Name").isEmpty()],
+   [check("name", "Please Provide Your Name").not().isEmpty()],
   async (req, res) => {
-    // const errors = validationResult(req);
-    // if (!errors.isEmpty()) {
-    //   return res.status(400).json({ errors: errors.array() });
-    // }
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
     const {
       name,
