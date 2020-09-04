c2646d83adc68191c6a7343569a662132887e7e6
Harshal Kaigaonkar <harshalkaigaonkar31@gmail.com>
2020-09-04 18:49:56 +0530

    update-3

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from "./Components/Pages/Home";
+
+const App = () => {
+  return (
+    <Router>
+      <div className='ma0 pa0'>
+        <Fragment>
+          <Switch>
+            <Route exact path='/' component={Home} />
+          </Switch>
+        </Fragment>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
