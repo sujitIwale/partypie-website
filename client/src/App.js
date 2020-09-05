52bc1ce06b98709428d376d8c798c044ed4ee1a2
sujitIwale <56259235+sujitIwale@users.noreply.github.com>
2020-09-05 21:04:13 +0530

    header-added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import './App.css';
 
 import Home from "./Components/Pages/Home";
 
 const App = () => {
   return (
     <Router>
-      <div className='ma0 pa0'>
+      <div className='ma0 pa0' >
         <Fragment>
           <Switch>
             <Route exact path='/' component={Home} />
