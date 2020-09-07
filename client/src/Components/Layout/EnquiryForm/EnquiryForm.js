1018b1b45277635e339f031c237184d905105f44
sujitIwale <56259235+sujitIwale@users.noreply.github.com>
2020-09-07 19:52:52 +0530

    enquiry-backend-connected

--- client/src/Components/Layout/EnquiryForm/EnquiryForm.js
+++ client/src/Components/Layout/EnquiryForm/EnquiryForm.js
@@ -1,50 +1,89 @@
-import React from 'react';
+import React,{useState} from 'react';
+import axios from 'axios'
 import './EnquiryForm.css';
 
+
 const EnquiryForm = () => {
+    const [Enquiry, setEnquiry] = useState({
+        name:'',
+        email:'',
+        phone:'',
+        date:'',
+        venue:'',
+        description:''
+    })
+    const {name,email,phone,date,venue,description} = Enquiry;
+    const onChange =(e) => {
+       setEnquiry({...Enquiry, [e.target.name]:e.target.value})
+    }
+
+    const onSubmit = async(e) => {
+       
+        e.preventDefault()
+        const enquiry = {
+            name:name,
+            email:email,
+            phone:phone,
+            eventDate:date,
+            eventType:'wedding',
+            eventDesc:description,
+            eventVenue:venue,
+        }
+        console.log(enquiry)
+        const config = {
+            headers:{
+                "Content-Type":"application/json"
+            }
+        }
+         const res = await axios.post('http://localhost:5000/api/addenquiries' , enquiry,config)
+         if (res) {
+             alert('enquiry send')
+         }
+         
+    }
     return (
         <div>
             <div className="container-contact100">
 		<div className="wrap-contact100">
-			<form className="contact100-form validate-form">
+			<form className="contact100-form validate-form" onSubmit={onSubmit}>
 				<span className="contact100-form-title">
 					Send Us A Message
 				</span>
 
 				<label className="label-input100" for="first-name">Tell us your name *</label>
 				<div className="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Type Your Name">
-					<input id="name" className="input100" type="text" name="first-name" placeholder="Name" />
+					<input id="name" className="input100" type="text" name="name" placeholder="Name" onChange={onChange} />
 					
 				</div>
 				
 
 				<label className="label-input100" for="email">Enter your email *</label>
 				<div className="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-					<input id="email" className="input100" type="text" name="email" placeholder="Eg. example@email.com" />
+					<input id="email" className="input100" type="text" name="email" placeholder="Eg. example@email.com" onChange={onChange}/>
 					<span className="focus-input100"></span>
 				</div>
 
 				<label className="label-input100" for="phone">Enter phone number</label>
 				<div className="wrap-input100">
-					<input id="phone" className="input100" type="text" name="phone" placeholder="Eg. +1 800 000000" />
+					<input id="phone" className="input100" type="text" name="phone" placeholder="Eg. +1 800 000000" onChange={onChange} />
 					<span className="focus-input100"></span>
 				</div>
 
                 <label className="label-input100" for="date">Event Date</label>
 				<div className="wrap-input100">
-					<input id="date" className="input100" type="date" name="date" placeholder={new Date()} />
+					<input id="date" className="input100" type="date" name="date" placeholder={new Date()} onChange={onChange}/>
 					<span className="focus-input100"></span>
 				</div>
                 
                 <label className="label-input100" for="Event Venue">Event Venue</label>
 				<div className="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Type Venue">
-					<input id="Venue" className="input100" type="text" name="venue" placeholder="Enter Venue" />
+					<input id="Venue" className="input100" type="text" name="venue" placeholder="Enter Venue" onChange={onChange} />
 					
 				</div>
 
 				<label className="label-input100" for="message">Event Discription </label>
 				<div className="wrap-input100 validate-input" data-validate = "Description is required">
-					<textarea id="description" className="input100" name="description" placeholder="Write a Event Description"></textarea>
+					<textarea id="description" className="input100" name="description" placeholder="Write a Event Description" onChange={onChange}></textarea>
 					<span className="focus-input100"></span>
 				</div>
 
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const cors = require('cors')
+
+
 
 const app = express();
+app.use(cors())
 app.use(express.json({ extended: false }));
 // Connect Database
 connectDB();
